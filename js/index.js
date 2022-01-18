9b878e4b262f67cb9ac129172ce4cc188c33f7cb
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-17 19:23:02 -0600

    comparando ganador

--- js/index.js
+++ js/index.js
@@ -4,5 +4,10 @@ var randomNumber2=Math.round((Math.random()*5)+1)
 document.querySelector(".dice .img1").setAttribute("src","images/dice"+randomNumber1+".png");
 document.querySelector(".dice .img2").setAttribute("src","images/dice"+randomNumber2+".png");
 
-
-console.log(randomNumber1);
+if(randomNumber1<randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML="Player 2 Wins! 🏆"
+}else if(randomNumber1>randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML="🏆 Player 1 Wins!"
+}else{
+    document.getElementsByTagName("h1")[0].innerHTML="🏆 Draw! 🏆"
+}
