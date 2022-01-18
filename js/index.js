9e83fa71b3237e8d58c09d40850795e8e209f8ba
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-17 19:11:21 -0600

    dados aleatorios

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 var randomNumber1=Math.round((Math.random()*5)+1)
+var randomNumber2=Math.round((Math.random()*5)+1)
+
+document.querySelector(".dice .img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".dice .img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
 
 console.log(randomNumber1);
